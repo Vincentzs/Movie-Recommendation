5a89c79ceb35e94185f81b6d58a760a0d017b241
Vincent Zhu <vincenthero16@gmail.com>
2024-04-18 14:05:59 -0400

    broken code

--- recommender.py
+++ recommender.py
@@ -13,6 +13,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import os
 import wandb
+import h5py
 
 class DRRAgent:
     
@@ -54,14 +55,44 @@ class DRRAgent:
         self.actor = Actor(self.embedding_dim, self.actor_hidden_dim, self.actor_learning_rate, state_size, self.tau)
         self.critic = Critic(self.critic_hidden_dim, self.critic_learning_rate, self.embedding_dim, self.tau)
         
+        # Adjust user & item dimensions based on the weights
+        if os.path.exists(weights_path) and load_weights:
+            with h5py.File(weights_path, 'r') as f:
+                # Get dimension
+                user_weights_shape = f['user_embedding/embeddings'].shape[0]
+                item_weights_shape = f['movie_embedding/embeddings'].shape[0]
+                users_num = user_weights_shape
+                items_num = item_weights_shape
+
+        self.users_num = users_num
+        self.items_num = items_num
+        
         # self.m_embedding_network = MovieGenreEmbedding(items_num, 19, self.embedding_dim)
         # self.m_embedding_network([np.zeros((1,)),np.zeros((1,))])
         # self.m_embedding_network.load_weights('/home/diominor/Workspace/DRR/save_weights/m_g_model_weights.h5')
 
-        self.embedding_network = UserMovieEmbedding(users_num, items_num, self.embedding_dim)
-        self.embedding_network([np.zeros((1,)),np.zeros((1,))])
+        # Original without weights
+        # self.embedding_network = UserMovieEmbedding(users_num, items_num, self.embedding_dim)
+        # self.embedding_network([np.zeros((1,)),np.zeros((1,))])
+        
+        # New: Initialize with adjusted dimension
+        self.embedding_network = UserMovieEmbedding(self.users_num, self.items_num, self.embedding_dim)
+        self.embedding_network.build(input_shape=[(None, 1), (None, 1)])
         # self.embedding_network = UserMovieEmbedding(users_num, self.embedding_dim)
         # self.embedding_network([np.zeros((1)),np.zeros((1,100))])
+        
+        if os.path.exists(weights_path):
+            loaded_shapes = np.load(weights_path, allow_pickle=True).item().keys()
+            model_shapes = {w.name: w.shape for w in self.embedding_network.trainable_weights}
+            shape_mismatch = any(model_shapes[w_name] != w_shape for w_name, w_shape in loaded_shapes)
+            if shape_mismatch:
+                print("Dimension/Shape mismacth")
+            else:
+                self.embedding_network.load_weights(weights_path)
+                print("Weights load success")
+        else:
+            print(f"No weight path {weights_path} found")
+        
         self.save_model_weight_dir = f"./save_model/trail-{datetime.now().strftime('%Y-%m-%d-%H')}"
         if not os.path.exists(self.save_model_weight_dir) or not load_weights:
             os.makedirs(os.path.join(self.save_model_weight_dir, 'images'))
